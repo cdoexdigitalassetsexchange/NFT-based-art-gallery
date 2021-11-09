72cea0f65c18f45c6d61241069015f8cf38bef66
Niftiart <93954098+niftiart@users.noreply.github.com>
2021-11-09 02:51:43 +0200

    Create NFTMarketplaceAuction.sol

---
+++ NFTMarketplaceAuction.sol
@@ -0,0 +1,116 @@
+contract  MarketplaceAuction {
+
+
+    bytes4 constant InterfaceSignature_ERC721 = bytes4(0x9a20483d);
+
+    
+    function MarketplaceConstruct(address _nftAddress, uint256 _cut) public {
+        require(_cut <= 10000);
+        ownerCut = _cut;
+
+        ERC721 candidateContract = ERC721(_nftAddress);
+        require(candidateContract.supportsInterface(InterfaceSignature_ERC721));
+        nonFungibleContract = candidateContract;
+    }
+
+    /// @dev Remove all Ether from the contract, which is the owner's cuts
+    ///  as well as any Ether sent directly to the contract address.
+    ///  Always transfers to the NFT contract, but can be called either by
+    ///  the owner or the NFT contract.
+    function withdrawBalance() external {
+        address nftAddress = address(nonFungibleContract);
+
+        require(
+            msg.sender == owner ||
+            msg.sender == nftAddress
+        );
+        
+        bool res = nftAddress.send(this.balance);
+    }
+
+
+    function createAuction(
+        uint256 _tokenId,
+        uint256 _startingPrice,
+        uint256 _endingPrice,
+        uint256 _duration,
+        address _seller
+    )
+        external
+        
+    {
+        
+        require(_startingPrice == uint256(uint128(_startingPrice)));
+        require(_endingPrice == uint256(uint128(_endingPrice)));
+        require(_duration == uint256(uint64(_duration)));
+
+        require(_owns(msg.sender, _tokenId));
+        _escrow(msg.sender, _tokenId);
+        Auction memory auction = Auction(
+            _seller,
+            uint128(_startingPrice),
+            uint128(_endingPrice),
+            uint64(_duration),
+            uint64(now)
+        );
+        _addAuction(_tokenId, auction);
+    }
+
+
+    function bid(uint256 _tokenId)
+        external
+        payable
+        
+    {
+        
+        _bid(_tokenId, msg.value);
+        _transfer(msg.sender, _tokenId);
+    }
+
+
+    function cancelAuction(uint256 _tokenId)
+        external
+    {
+        Auction storage auction = tokenIdToAuction[_tokenId];
+        require(_isOnAuction(auction));
+        address seller = auction.seller;
+        require(msg.sender == seller);
+        _cancelAuction(_tokenId, seller);
+    }
+
+    
+
+    function getAuction(uint256 _tokenId)
+        external
+        view
+        returns
+    (
+        address seller,
+        uint256 startingPrice,
+        uint256 endingPrice,
+        uint256 duration,
+        uint256 startedAt
+    ) {
+        Auction storage auction = tokenIdToAuction[_tokenId];
+        require(_isOnAuction(auction));
+        return (
+            auction.seller,
+            auction.startingPrice,
+            auction.endingPrice,
+            auction.duration,
+            auction.startedAt
+        );
+    }
+
+    
+    function getCurrentPrice(uint256 _tokenId)
+        external
+        view
+        returns (uint256)
+    {
+        Auction storage auction = tokenIdToAuction[_tokenId];
+        require(_isOnAuction(auction));
+        return _currentPrice(auction);
+    }
+
+}
